0c09e2239c727a4f1758e20bdaf62f245f3dc998
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-23 12:41:00 +0530

    wordCounter testing

--- src/__tests__/wordCounter.test.tsx
+++ src/__tests__/wordCounter.test.tsx
@@ -1,6 +1,13 @@
-import { render } from "@testing-library/react"
-import Home from "../pages/Home"
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import WordCounter from "../components/home/WordCounter";
 
 test("Renders the wordCounter component", () => {
-    render(<Home/>)
-})
+  render(<WordCounter wordsCount={10} charactersCount={50} />);
+
+  const wordCount = screen.getByText("10 Words")
+  const characterCount = screen.getByText("50 Characters")
+
+  expect(wordCount).toBeInTheDocument();
+  expect(characterCount).toBeInTheDocument();
+});
