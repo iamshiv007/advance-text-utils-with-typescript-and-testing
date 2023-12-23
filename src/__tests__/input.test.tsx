f8230f4599710b7f155f09aef5e1f39a2954802a
iamshiv007 <iamshiv20032003@gmail.com>
2023-12-23 12:37:58 +0530

    textarea input testing

--- src/__tests__/input.test.tsx
+++ src/__tests__/input.test.tsx
@@ -1,10 +1,26 @@
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react';
-import Input from '../components/home/Input';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Input from "../components/home/Input";
 
-test('Renders the Input component', () => {
+test("test the Input component", () => {
   const setWordsCountMock = jest.fn();
   const setCharactersCountMock = jest.fn();
 
-  render(<Input setWordsCount={setWordsCountMock} setCharactersCount={setCharactersCountMock} />)
+  render(
+    <Input
+      setWordsCount={setWordsCountMock}
+      setCharactersCount={setCharactersCountMock}
+    />
+  );
+
+  let input = screen.getByRole("textbox")as HTMLTextAreaElement;
+  expect(input).toBeInTheDocument();
+
+  input = screen.getByPlaceholderText(
+    "Start typing, or copy and paste your document here..."
+  ) as HTMLTextAreaElement;
+  expect(input).toBeInTheDocument();
+
+  fireEvent.change(input, { target: { value: "abc" } });
+  expect(input.value).toBe("abc");
 });
